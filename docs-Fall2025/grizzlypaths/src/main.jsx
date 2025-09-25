292bc4f345fcc0146a7441be3c849d08eb4435e6
William <wchokbengboune@ggc.edu>
2025-09-25 16:57:22 -0400

    adding index.js and updating package and main for firebase and vite

--- docs-Fall2025/grizzlypaths/src/main.jsx
+++ docs-Fall2025/grizzlypaths/src/main.jsx
@@ -1,6 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+//imports firebase to folder
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 // Import Bootstrap and your custom CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './interfaceSettings.css';
