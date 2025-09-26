febf7bc9ea7678e7933f84b69d02653ea9a6c944
William <wchokbengboune@ggc.edu>
2025-09-26 18:17:51 -0400

    revert to prevent breaking from firebase

--- docs-Fall2025/grizzlypaths/src/main.jsx
+++ docs-Fall2025/grizzlypaths/src/main.jsx
@@ -1,10 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-//imports firebase to folder
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
 // Import Bootstrap and your custom CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './interfaceSettings.css';
