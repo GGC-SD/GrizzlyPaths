c8509e0a1c1ca30aeb83595337031cd61b67c12b
hieudo2604 <trunghieu26042003@gmail.com>
2025-09-26 20:53:24 -0400

    Create main.jsx

---
+++ docs-Fall2025/grizzlypaths/src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// Import Bootstrap and your custom CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './interfaceSettings.css';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <App />
+);
