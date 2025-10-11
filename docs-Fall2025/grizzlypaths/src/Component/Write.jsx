06cfa466800fc2f4c52deeb7d2713516ac0948c0
William <wchokbengboune@ggc.edu>
2025-10-11 12:45:49 -0400

    placed routing in folder

---
+++ docs-Fall2025/grizzlypaths/src/Component/Write.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import { app } from "../firebase";
+import { getDatabase, ref, set, push } from "firebase/database";
+
+function Write(){
+    return(
+        <div>
+            Write
+        </div>
+    )
+}
+
+export default Write;
